c735df433fe79bcb17b5f27863678b8a92e57a4c
tommy <tom.faucheux@mailo.com>
2026-05-13 16:59:18 +0200

    test

--- 30DaysOfStreamlit.py
+++ 30DaysOfStreamlit.py
@@ -3,25 +3,21 @@ import altair as alt
 import pandas as pd
 import streamlit as st
 
-# Cette commande définit le titre de l'onglet du navigateur
 st.set_page_config(
     page_title="Intro à Streamlit",
 )
 
-st.title("Intro à Streamlit") # Ceci est le header (H1) dans la page
+st.title("Intro à Streamlit")
 st.markdown("[30 Days of Streamlit](https://30days.streamlit.app/)")
 
-st.header('st.slider')
-st.subheader('st.slider 1')
+st.header('slider')
+st.subheader('slider 1')
 
 st.slider("",0,130,25)
 
+st.subheader('slider 2')
 
-values = st.slider(
-     '',
-     0.0, 100.0, (25.0, 75.0))
-st.write('', values)
-
+st.slider('',0.0, 100.0, (25.0, 75.0))
 
 
 chart_data = pd.DataFrame(
@@ -29,3 +25,8 @@ chart_data = pd.DataFrame(
      columns=['a', 'b', 'c'])
 
 st.line_chart(chart_data)
+
+
+st.selectbox("selectbox",(1,2))
+
+st.multiselect("multiselect",(1,2))
