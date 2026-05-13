feb1792c1314fa7fd2c3a968b62e82fd291ca308
tommy <tom.faucheux@mailo.com>
2026-05-13 15:46:26 +0200

    test

---
+++ 9_line_chart.py
@@ -0,0 +1,11 @@
+import numpy as np
+import altair as alt
+import pandas as pd
+import streamlit as st
+
+# Cette commande définit le titre de l'onglet du navigateur
+st.set_page_config(
+    page_title="9_line_chart",
+)
+
+
